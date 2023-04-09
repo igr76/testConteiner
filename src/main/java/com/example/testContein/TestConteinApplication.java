d0f44023020a8ffa79df9b60a1bc4c96a06527d4
igr76 <gritsuk76@mail.ru>
2023-04-09 16:15:50 +1000

    start

---
+++ src/main/java/com/example/testContein/TestConteinApplication.java
@@ -0,0 +1,13 @@
+package com.example.testContein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testContein/TestConteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testContein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
